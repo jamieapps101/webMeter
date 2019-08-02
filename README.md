e9554e74c7d6f388d2578dcafa2e57d034f8549a
jamie <jamieapps101@gmail.com>
2019-08-02 23:43:44 +0100

    setup files + a little bulking
    webpage mostly setup
    js half done
    good link to websocket stuff noted in esp8266 file
    To be done is listed in the readme file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-webMeter
+**webMeter Project**
+======================
+
+Simple project polling an esp8266 for data so as to act
+as a detached meter for physical readings. Using a web interface, a live readout shall be given as well as a plotted summary of the previous readings.
+
+
+To Do Soon:
+* Create JS to attach to controls section
+* Add sanity checks/validation to that
+* Add some CSS to actually make the thing look useable
+* Rearrange bottom inputs into a grid
+* Add the graphing functionality using plotly.js
+* create a repeat polling function/look into sockets with an interrupting function to allow refresh on update
+* Build hardware
+* write firmware
+* test
+
+To Do At Some Point:
+* make it mobile friendly
+* export graph
+* add server style script for longer data logging
+* add battery measurements/estimates to monitor hardware health
+* add cookies to remember previous device IP, update rate and unit settings
---
+++ client/functions.js
@@ -0,0 +1,68 @@
+var time = 0;
+var unitsString = "null";
+var updateRate = 1000; // ie every 1000ms
+var devIP = "null"; // who knows that this will be
+var scaleUnits = 1.00;
+var intervalID = setInterval(pollDevice, updateRate);
+
+var socketConnected = 0; // used to show not connected on live val but zeros on graph
+
+var websocket = null;
+// put a check for a cookie here, and if present attempt to connect to the
+// device
+
+
+function pollDevice()
+{
+  //console.log("Time: " + time.toString());
+  var d = new Date();
+  document.getElementById("val").innerHTML = "<h3>" +
+  d.getHours().toString() + ":" +
+  d.getMinutes().toString() + ":" +
+  d.getSeconds().toString() +
+  unitsString + "</h3>";
+  time = time + 1;
+}
+
+function setDevIP()
+{
+  devIP = document.getElementById('ipInput').value;
+  console.log("setting dev IP: " + devIP);
+
+  if(false) // check to see if a valid ip was entered
+  {
+
+  }
+}
+
+function setUpdateRate()
+{
+  updateRate = parseInt(document.getElementById('rateInput').value);
+  console.log("setting update rate: " +
+  updateRate.toString() + "ms");
+  clearInterval(intervalID);
+  intervalID = setInterval(pollDevice, updateRate);
+}
+
+function setUnits()
+{
+  unitsString = document.getElementById('unitInput').value;
+  console.log("setting units: " + unitsString);
+}
+
+function setScaling()
+{
+  scaleUnits = parseFloat(document.getElementById('scaleInput').value);
+  console.log("setting scale: " + scaleUnits);
+}
+
+function webSocketOpened(event)
+{
+  socketConnected = 1;
+  console.log("Websocket connected");
+}
+function webSocketClosed(event)
+{
+  socketConnected = 0;
+  console.log("websocket disconnected");
+}
